5d1b20c24a4ac11662b5c7157bfaa5b0dd7219f5
Mark Armendariz <armendariz.mark@yahoo.om>
2019-09-24 22:43:40 -0700

    Initial commit of React app

---
+++ src/App.js
@@ -0,0 +1,33 @@
+import React from 'react';
+import logo from './logo.svg';
+import './App.scss';
+import NavBar from './shared-components/nav-bar';
+import Footer from './shared-components/footer';
+import Paper from '@material-ui/core/Paper';
+
+import Grid from '@material-ui/core/Grid';
+import {makeStyles} from '@material-ui/core/styles';
+
+class App extends React.Component {
+  render() {
+    return (
+      <div id="app">
+        <Grid container space={3}>
+          <Grid item xs={12}>
+            <NavBar></NavBar>
+          </Grid>
+          <Grid item xs={12}>
+            <main id="test">
+              MAIN
+            </main>
+          </Grid>
+          <Grid item xs={12}>
+            <Footer id="app-footer"></Footer>
+          </Grid>
+        </Grid>
+      </div>
+    );
+  }
+}
+
+export default App;
---
+++ src/shared-components/footer/Footer.js
@@ -0,0 +1,62 @@
+import React from 'react';
+import './Footer.scss';
+import Grid from '@material-ui/core/Grid';
+
+class Footer extends React.Component{
+  render(){
+    return (
+      <div id="app-footer">
+        <section id="connection-secton">
+
+        </section>
+
+        <Grid id="grid-wrapper" container>
+          <Grid item xs={2}></Grid>
+          <Grid item xs={2}>
+            <h5>Skyrth, LLC.</h5>
+            <hr></hr>
+            <p>"At vero eos et accusamus et iusto odio dignissimos 
+              ducimus qui blanditiis praesentium voluptatum deleniti 
+              atque corrupti quos dolores et quas molestias excepturi 
+            </p>
+          </Grid>
+          <Grid item xs={2}>
+            <h5>Section Title</h5>
+            <hr></hr>
+            <ul>
+              <li>Link 1</li>
+              <li>Link 2</li>
+              <li>Link 3</li>
+              <li>Link 4</li>
+            </ul>
+          </Grid>
+          <Grid item xs={2}>
+            <h5>Section Title</h5>
+            <hr></hr>
+            <ul>
+              <li>Link 1</li>
+              <li>Link 2</li>
+              <li>Link 3</li>
+              <li>Link 4</li>
+            </ul>
+          </Grid>
+          <Grid item xs={2}>
+            <h5>Section Title</h5>
+            <hr></hr>
+            <ul>
+              <li>Link 1</li>
+              <li>Link 2</li>
+              <li>Link 3</li>
+              <li>Link 4</li>
+            </ul>
+          </Grid>
+          <Grid item xs={2}></Grid>
+        </Grid>
+        <section id="copyright">
+          <p>&copy; 2019 Copyright <span id="text-highlight">Skyrth, LLC.</span></p>
+        </section>
+      </div>
+      );
+  }
+}
+export default Footer;
